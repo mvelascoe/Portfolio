6eb66244a00dffdf54f60c6ac519066729e66932
marivelascoe <maritza.94v@gmail.com>
2024-09-01 20:36:11 -0500

    feat: :sparkles: Se realiza la seccion Home con sus stilos - Se definen los colores a usar - Se realiza para que sea una pagina responsive

---
+++ js/main.js
@@ -0,0 +1,35 @@
+
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-abrir'),
+      navClose = document.getElementById('nav-cerrar')
+
+if(navToggle){
+   navToggle.addEventListener('click', () =>{
+      navMenu.classList.add('show-menu')
+   })
+}
+
+
+if(navClose){
+   navClose.addEventListener('click', () =>{
+      navMenu.classList.remove('show-menu')
+   })
+ }
+
+
+const navLink = document.querySelectorAll('.nav__link')
+const linkAction = () =>{
+   const navMenu = document.getElementById('nav-menu')
+
+   navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+const blurHeader = () =>{
+   const header = document.getElementById('header')
+   this.scrollY >= 50 ? header.classList.add('blur-header') 
+                      : header.classList.remove('blur-header')
+}
+
+window.addEventListener('scroll', blurHeader)
